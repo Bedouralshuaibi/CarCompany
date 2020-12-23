b26065ff8550ae87e3ec0f5f4927b84887377860
Bedour Alshuaibi <b.f.alshuaibi@com>
2020-12-23 17:23:16 +0300

    frontend

---
+++ carFront/carcompany/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+        
+        </p>
+        
+         Start coding BEDOUR !!!!
+        
+      </header>
+      <body>
+        <p>hi lets do somthing</p>
+      </body>
+    </div>
+    
+  );
+}
+
+export default App;
